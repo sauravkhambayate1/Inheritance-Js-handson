2964456915acf3eb6a1a9811ac8219175b3a1745
Saurav Kiran Khambayate <sauravkhambayate122@gmail.com>
2022-12-22 18:10:11 +0530

    Inheritance

---
+++ main.js
@@ -0,0 +1,60 @@
+// 1.solutions
+
+const parent = {
+    firstName: `nouman`,
+    lastName: `mullaji`,
+    fullname() {
+      return (`${this.firstName} ${this.lastName}`)
+    }
+  
+  }
+  
+  const child = {
+    __proto__: parent
+  }
+  console.log(child.fullname());
+  // 2.solutions
+  
+  function ABC() {
+    this.firstName = "nouman"
+    this.lastName = "mullaji"
+  
+    ABC.prototype.age = "23"
+    ABC.prototype.hobby = "cricket,tracking"
+  
+  }
+  const newPerson = new ABC();
+  console.log(newPerson.firstName);
+  console.log(newPerson.lastName);
+  console.log(newPerson.age);
+  console.log(newPerson.hobby);
+  
+  //3.solution
+  const arr = [1, 2, 3, 4, 5, 6]
+  const arr1 = [8, 7, 9, 4, 3, 2]
+  const arr2 = [2, 3, 4, 9, 7, 8]
+  const sumOfArray = {
+    addAll: function (arr) { // create a method inside an object to any multiple array
+      let sum = 0
+      for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i]
+      }
+      console.log(sum);
+    }
+  }
+  
+  Array.__proto__ = sumOfArray;
+  sumOfArray.addAll(arr)
+  sumOfArray.addAll(arr1)
+  sumOfArray.addAll(arr2)
+  
+  //4.solution
+  var obj = {
+    name: "nouman",
+    course: `full-stack`,
+    collge: `GIT`
+  
+  }
+  
+  const result = Object.keys(obj)
+  console.log(result);
